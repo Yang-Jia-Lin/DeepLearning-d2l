c6edb347962dde124ee4a9c34147370fd40f6f43
yangjialin <2239969828@qq.com>
2024-05-24 00:12:29 +0800

    统一框架格式

--- my_d2l/net_model.py
+++ my_d2l/net_model.py
@@ -3,19 +3,19 @@ from torch import nn
 
 
 def linreg(X, W, b):
-    """线性回归模型——1层——全连接层"""
+    """线性回归网络——1层——全连接层"""
     return torch.matmul(X, W) + b
 
 
 def softmax(X):
-    """softmax模型——1层——softmax层"""
+    """softmax层"""
     X_exp = torch.exp(X)
     partition = X_exp.sum(1, keepdim=True)
     return X_exp / partition  # 广播机制
 
 
 def linClassify(X, W, b):
-    """线性分类模型——2层——全连接层+softmax层"""
+    """线性分类网络——2层——全连接层+softmax层"""
     return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
 
 
