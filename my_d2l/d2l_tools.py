fbabc0a92598a843f8e65a596717222d6baac54f
yangjialin <2239969828@qq.com>
2024-05-24 11:38:33 +0800

    统一框架格式

--- my_d2l/d2l_tools.py
+++ my_d2l/d2l_tools.py
@@ -1,5 +1,4 @@
 import torch
-from IPython import display
 from d2l import torch as d2l
 
 
@@ -27,57 +26,26 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
     return axes
 
 
-class Animator:
-    """训练过程动画显示"""
-    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
-                 ylim=None, xscale='linear', yscale='linear',
-                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
-                 figsize=(3.5, 2.5)):
-        # Incrementally plot multiple lines
-        if legend is None:
-            legend = []
-        d2l.use_svg_display()
-        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
-        if nrows * ncols == 1:
-            self.axes = [self.axes, ]
-        # Use a lambda function to capture arguments
-        self.config_axes = lambda: d2l.set_axes(
-            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
-        self.X, self.Y, self.fmts = None, None, fmts
-
-    def add(self, x, y):
-        # Add multiple data points into the figure
-        if not hasattr(y, "__len__"):
-            y = [y]
-        n = len(y)
-        if not hasattr(x, "__len__"):
-            x = [x] * n
-        if not self.X:
-            self.X = [[] for _ in range(n)]
-        if not self.Y:
-            self.Y = [[] for _ in range(n)]
-        for i, (a, b) in enumerate(zip(x, y)):
-            if a is not None and b is not None:
-                self.X[i].append(a)
-                self.Y[i].append(b)
-        self.axes[0].cla()
-        for x, y, fmt in zip(self.X, self.Y, self.fmts):
-            self.axes[0].plot(x, y, fmt)
-        self.config_axes()
-        display.display(self.fig)
-        display.clear_output(wait=True)
-
-
 class Accumulator:
     """对 n个变量求和"""
     def __init__(self, n):
         self.data = [0.0] * n
 
     def add(self, *args):
+        # 将data中的累积值和args传入的新值按位置配对
         self.data = [a + float(b) for a, b in zip(self.data, args)]
+        # 解释：
+        # 当self.data=[1,1], args=[3,5]时, zip=[(1,3),(1,5)];
+        # 当for循环时, 每个元素是一个二元元组(a,b), a是原始累计值, b是传入新值
+        # 最后使用列表推导 [a + float(b) for …] 组成一个新的数组，与原始数组大小相同
 
     def reset(self):
         self.data = [0.0] * len(self.data)
 
     def __getitem__(self, idx):
         return self.data[idx]
+
+
+argmax = lambda x, *args, **kwargs: x.argmax(*args, **kwargs)
+astype = lambda x, *args, **kwargs: x.type(*args, **kwargs) # 通用类型转换函数
+reduce_sum = lambda x, *args, **kwargs: x.sum(*args, **kwargs)
--- my_d2l/train_model.py
+++ my_d2l/train_model.py
@@ -2,65 +2,89 @@ import torch
 import data_preprocess
 import evaluate
 import d2l_tools
-# from d2l import torch as d2l
 
 
 # 线性回归
 def linear_scratch_train(num_epochs, batch_size, lr, features, labels, net, params, loss, updater):
+    """线性回归模型训练实现——手动"""
     for epoch in range(num_epochs):
+        # 模型训练
         for X, y in data_preprocess.data_iter_by_hand(batch_size, features, labels):
-            l = loss(net(X, params[0], params[1]), y)         # 正向传播
-            l.sum().backward()                  # 反向传播
-            updater(params, lr, batch_size)     # 更新参数
+            # 1. 正向传播+计算损失
+            l = loss(net(X, params[0], params[1]), y)
+            # 2. 反向传播
+            l.sum().backward()
+            # 3. 更新参数
+            updater(params, lr, batch_size)
+        # 模型评估
         with torch.no_grad():
             train_l = loss(net(features, params[0], params[1]), labels)
             print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
+    print('完成训练\n')
 
 
 def linear_concise_train(num_epochs, batch_size, features, labels, net, loss, updater):
+    """线性回归模型训练实现——自动"""
     for epoch in range(num_epochs):
+        # 模型训练
         for X, y in data_preprocess.load_array((features, labels), batch_size):
+            # 1.正向传播+计算损失
             l = loss(net(X), y)
+            # 2.反向传播（先将梯度清零）
             updater.zero_grad()
             l.backward()
+            # 3.更新参数
             updater.step()
+        # 模型评估
         l = loss(net(features), labels)
         print(f'epoch {epoch + 1}, loss {l:f}')
+    print('完成训练\n')
 
 
 # 线性分类
 def softmax_scratch_train(num_epochs, batch_size, lr, train_iter, test_iter, net, params, loss, updater):
-    """softmax训练"""
+    """线性分类模型训练实现——手动"""
     for epoch in range(num_epochs):
-        metric = d2l_tools.Accumulator(3)  # 训练损失总和、训练准确度总和、样本数
+        # 模型训练
+        metric = d2l_tools.Accumulator(3)  # 累计一个batch中损失总和、预测正确数量、总样本数
         for X, y in train_iter:
-            y_hat = net(X, params[0], params[1])    # 正向传播
-            l = loss(y_hat, y)      # 计算损失
-            l.sum().backward()      # 反向传播
-            updater(params, lr, batch_size)   # 更新参数
-            metric.add(float(l.sum()), evaluate.accuracy(y_hat, y), y.numel())
-        train_metrics = metric[0] / metric[2], metric[1] / metric[2]
-        print(f'epoch {epoch + 1}, loss {train_metrics[0]:.5f}, acc {train_metrics[1]:.5f}')
-    test_acc = evaluate.evaluate_accuracy_scratch(net, test_iter, params[0], params[1])
-    print(f'完成训练，在测试集上 acc {test_acc:.5f} \n')
+            # 1.正向传播
+            y_hat = net(X, params[0], params[1])
+            # 2.计算损失
+            l = loss(y_hat, y)  # 参与backward
+            # 3.反向传播
+            l.sum().backward()
+            # 4.更新参数
+            updater(params, lr, batch_size)
+            # 5.累加损失和精度
+            metric.add(float(l.sum()), evaluate.accuracy(y_hat, y), y.numel())  # 不参与backward
+        # 模型评估
+        train_metrics = metric[0] / metric[2], metric[1] / metric[2]  # 训练完一个epoch后的平均损失、预测准确率
+        test_acc = evaluate.evaluate_accuracy_scratch(net, test_iter, params[0], params[1])
+        print(f'epoch {epoch + 1}, loss {train_metrics[0]:.5f}, train_acc {train_metrics[1]:.5f}, test_acc {test_acc:.5f}')
+    print(f'完成训练\n')
 
 
 def softmax_concise_train(num_epochs, train_iter, test_iter, net, loss, updater):
-    """softmax训练"""
-    # animator = tools.Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],legend=['train loss', 'train acc', 'test acc'])
+    """线性分类模型训练实现——自动"""
     for epoch in range(num_epochs):
+        # 模型训练
         net.train()
-        metric = d2l_tools.Accumulator(3)  # 训练损失总和、训练准确度总和、样本数
+        metric = d2l_tools.Accumulator(3)  # 累计一个batch中损失总和、预测正确数量、总样本数
         for X, y in train_iter:
-            y_hat = net(X)  # 计算梯度并更新参数
+            # 1.正向传播
+            y_hat = net(X)
+            # 2.计算损失
             l = loss(y_hat, y)
+            # 3.反向传播
             updater.zero_grad()
             l.mean().backward()
+            # 4.更新参数
             updater.step()
-            metric.add(float(l.sum()), evaluate.accuracy(y_hat, y), y.numel())
-        train_metrics = metric[0] / metric[2], metric[1] / metric[2]
-        print(f'epoch {epoch + 1}, loss {train_metrics[0]:.5f}, acc {train_metrics[1]:.5f}')
-        # test_acc = tools.evaluate_accuracy(net, test_iter, W, b)
-        # animator.add(epoch + 1, train_metrics + (test_acc,))
-    test_acc = evaluate.evaluate_accuracy_concise(net, test_iter)
-    print(f'完成训练，在测试集上 acc {test_acc:.5f}')
+            # 5.累加损失和精度
+            metric.add(float(l.sum()), evaluate.accuracy(y_hat, y), y.numel())  # 不累计梯度
+        # 模型评估
+        train_metrics = metric[0] / metric[2], metric[1] / metric[2]  # 训练完一个epoch后的平均损失、预测准确率
+        test_acc = evaluate.evaluate_accuracy_concise(net, test_iter)
+        print(f'epoch {epoch + 1}, loss {train_metrics[0]:.5f}, train_acc {train_metrics[1]:.5f}, test_acc {test_acc:.5f}')
+    print('完成训练')
